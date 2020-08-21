7ee160b890fdc66ed661b2b3af344369512918e4
Jeff St. Jean <jeffrey@stjean.ca>
2020-08-21 02:39:15 -0400

    quick fix

--- cert/get_certificates.sh
+++ cert/get_certificates.sh
@@ -2,7 +2,8 @@
 
 # run this to get your first certificate (make sure nginx isn't currently running!)
 
-email=$1
+domain=$1
+email=$2
 
 docker run --rm -p 443:443 -p 80:80 --name letsencrypt -v "/etc/letsencrypt:/etc/letsencrypt" \
-    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certonly -n \ -m "$name" -d example.com --standalone --agree-tos
+    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certonly -n -m "$email" -d $domain --standalone --agree-tos
